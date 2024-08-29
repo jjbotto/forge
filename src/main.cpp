a7292a3da73389284afb95a418bf00486c60d80f
JJ Bottomley <jj@JJs-MacBook-Pro.attlocal.net>
2024-08-29 02:01:43 -0400

    added .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,7 @@ int main() {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
+
                 window.close();
             }
         }
